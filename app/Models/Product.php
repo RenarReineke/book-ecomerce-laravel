77a49d4212c3105bf5d4c4ca28fc2a2fdecea867
Renar <kondrashov.dimitr@gmail.com>
2023-07-04 13:04:50 +0300

    feat: add field  in Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,16 +11,21 @@ class Product extends Model
 
     protected $fillable = ['title', 'description', 'price'];
 
-    public function category() {
+    protected $with = ['category'];
+
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function rewiews() {
+    public function rewiews()
+    {
         return $this->hasMany(Rewiew::class);
     }
 
-    public function tags() {
+    public function tags()
+    {
         return $this->belongsToMany(Tag::class);
     }
-    
+
 }
