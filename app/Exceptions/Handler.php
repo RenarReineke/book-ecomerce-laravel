e8595fcc08d77679b2b90609c3ed2373ee0e81a5
Renar <kondrashov.dimitr@gmail.com>
2023-07-17 19:50:03 +0300

    feat: add render method in app/Exceptions/Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,10 +2,14 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Throwable;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
+
 {
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
@@ -26,5 +30,12 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exeption) 
+    {
+        if ($exeption instanceof ModelNotFoundException && $request->wantsJson()) {
+            return response()->json(['message' => 'Not found'], 404);
+        }
     }
 }
