f4d96a524965170a9bd533d292391332779087e5
Dmitrii <kondrashov.dimitr@gmail.com>
2023-08-27 18:51:41 +0300

    feat: add paginate for Category

--- app/Http/Controllers/CategoryResourceController.php
+++ app/Http/Controllers/CategoryResourceController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Category\StoreCategoryRequest;
+use App\Http\Requests\Category\UpdateCategoryRequest;
 use App\Http\Requests\CategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
@@ -14,14 +16,14 @@ class CategoryResourceController extends Controller
      */
     public function index()
     {
-        $categories = Category::get();
+        $categories = Category::paginate(10);
         return CategoryResource::collection($categories);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(CategoryRequest $request)
+    public function store(StoreCategoryRequest $request)
     {
         $category = Category::create($request->validated());
         return new CategoryResource($category);
@@ -39,7 +41,7 @@ class CategoryResourceController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(CategoryRequest $request, string $id)
+    public function update(UpdateCategoryRequest $request, string $id)
     {
         $category = Category::findOrFail($id);
         $category->update($request->validated());
